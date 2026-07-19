1f0250d0b38a46ee3e08b7868e274382721a1de2
pisces312 <nili66china@163.com>
2026-07-19 20:53:50 +0800

    chore: consolidate onto base conda env; update sender paths & env.yml
    - Point tasks.json/launch.json/settings.json/gen_and_display.bat to base python
    - env.yml: name=base, opencv>=5.0, drop unused qreader
    - qrcast_env merged into base and removed

--- sender/gen_and_display.bat
+++ sender/gen_and_display.bat
@@ -1,5 +1,5 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
-"D:\dev\conda_envs\qrcast_env\python.exe" -m qrcast.bw.gen_and_display_individual "%~1"
+"D:\dev\miniconda3\python.exe" -m qrcast.bw.gen_and_display_individual "%~1"
 pause
